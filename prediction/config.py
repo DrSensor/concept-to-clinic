396629f45306208b393650d8d02ff3ab709752a0
Willi Gierke <willi.gierke@student.hpi.de>
2017-10-14 13:24:14 +0200

    use __file__

--- prediction/config.py
+++ prediction/config.py
@@ -10,10 +10,12 @@ import os
 class Config(object):
     PROD_SERVER = os.getenv('PRODUCTION', False)
     DEBUG = False
-    SEGMENT_ASSETS_DIR = os.path.abspath(os.path.join(os.getcwd(), 'src', 'algorithms', 'segment', 'assets'))
+    # The following paths are expanded at runtime
+    CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+    SEGMENT_ASSETS_DIR = os.path.abspath(os.path.join(CURRENT_DIR, 'src', 'algorithms', 'segment', 'assets'))
     DICOM_PATHS_DOCKER_WILDCARD = os.path.join('/images_full', 'LIDC-IDRI-*', '**', '**')
-    DICOM_PATHS_LOCAL_WILDCARD = os.path.join(os.getcwd(), 'src', 'tests', 'assets', 'test_image_data', 'full',
-                                              'LIDC-IDRI-*', '**', '**')
+    DICOM_PATHS_LOCAL_WILDCARD = os.path.join(CURRENT_DIR, 'src', 'tests', 'assets',
+                                              'test_image_data', 'full', 'LIDC-IDRI-*', '**', '**')
 
 
 class Production(Config):
